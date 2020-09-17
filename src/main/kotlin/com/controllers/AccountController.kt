886210ba8ce6f1d02c18741917bc33b2c621c996
richardscheid <rscheid@live.com>
2020-09-17 17:14:14 -0300

    chore: add request mapping

--- src/main/kotlin/com/controllers/AccountController.kt
+++ src/main/kotlin/com/controllers/AccountController.kt
@@ -3,13 +3,13 @@ package com.controllers
 import com.models.Account
 import com.services.AccountService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/account")
 class AccountController(val accountService: AccountService) {
 
     @GetMapping("/find")
-    fun findAll(): List<Account> {
-        return accountService.getAll()
-    }
+    fun findAll(): List<Account> = accountService.getAll()
 }
